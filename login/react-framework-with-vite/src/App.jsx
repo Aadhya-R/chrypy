acf7a1fc979fd184caeb21ff3c6f86ae5c36ed6e
Aadhya-R <aadhyar.cs24@rvce.edu.in>
2025-09-02 18:34:32 +0530

    Fixing or and visibility symbol

--- login/react-framework-with-vite/src/App.jsx
+++ login/react-framework-with-vite/src/App.jsx
@@ -1,17 +1,56 @@
-import SocialLogin1 from "./components/SocialLogin1";
-import InputField from "./components/InputField";
+import React, { useState } from 'react';
+
+// Reusing the InputField component structure for consistent styling
+const InputField = ({ type, placeholder, icon }) => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  return (
+    <div className="input-wrapper">
+      <input
+        type={isPasswordShown ? 'text' : type}
+        placeholder={placeholder}
+        className="input-field"
+        required
+      />
+      <i className="material-symbols-rounded">{icon}</i>
+      {type === 'password' && (
+        <i onClick={() => setIsPasswordShown(prevState => !prevState)} className="material-symbols-rounded eye-icon">
+          {isPasswordShown ? 'visibility' : 'visibility_off'}
+        </i>
+      )}
+    </div>
+  );
+};
+
+// Reusing the SocialLogin component
+const SocialLogin1 = () => {
+  return (
+    <div className="social-login">
+      <button className="social-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="white">
+          <path d="M12.24 10.285V14.45h6.874c-.292 1.344-1.636 4.148-6.721 4.148-4.148 0-7.5-3.351-7.5-7.5s3.351-7.5 7.5-7.5c2.257 0 3.633.954 4.542 1.815l3.221-3.221C18.675 1.54 15.658 0 12.24 0c-6.671 0-12.083 5.412-12.083 12.083s5.412 12.083 12.083 12.083c6.918 0 11.666-4.992 11.666-11.758 0-.82-.12-1.554-.282-2.323H12.24zm0 0" />
+        </svg>
+        Google
+      </button>
+      <button className="social-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="white">
+          <path d="M21.5 14.5c.5-.7 1.3-1.2 2.5-1.2 0-2.5-2.2-4.1-4.9-4.2-2.3 0-4.1 1.2-5.1 1.2s-2.7-1.2-5.1-1.2c-2.7 0-4.5 1.7-4.9 4.2-.7 0-1.2.7-1.2 1.2s.5 1.2 1.2 1.2h.1c.3 1.5 1.5 3.3 3.6 3.3 2.1 0 3.4-1.8 5.4-1.8s3.3 1.8 5.4 1.8c2.1 0 3.3-1.8 3.6-3.3h.1c.7 0 1.2-.5 1.2-1.2s-.5-1.2-1.2-1.2zm-10.8-6.1c.9-1.2 2.3-2 4-2s3.1.8 4 2c-.9 0-2.3.6-4 1.4s-3.1-1-4-1.4z" />
+        </svg>
+        Apple
+      </button>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <>
-      <div className="title-container">
-        <h1 className="chyrp-title">     Chyrp Lite</h1>
-      </div>
+      <h1 className="chyrp-title">Chyrp Lite</h1>
       <div className="login-container">
         <h2 className="form-title">Log in with</h2>
         <SocialLogin1 />
 
-        <p className="separator"></p><span>or</span>
+        <p className="separator"><span>or</span></p>
 
         <form action="#" className="login-form">
           <InputField type="email" placeholder="Email address" icon="mail" />
@@ -26,7 +65,7 @@ const App = () => {
         </p>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default App;
