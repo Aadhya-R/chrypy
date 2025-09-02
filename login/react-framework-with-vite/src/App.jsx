3240046a80be1ce5159f7bc0ca81c0a6f93aee5e
Aadhya-R <aadhyar.cs24@rvce.edu.in>
2025-09-02 14:55:47 +0530

    debugged login page

--- login/react-framework-with-vite/src/App.jsx
+++ login/react-framework-with-vite/src/App.jsx
@@ -1,26 +1,29 @@
-import SocialLogin from "./components/SocialLogin1";
+import SocialLogin1 from "./components/SocialLogin1";
 import InputField from "./components/InputField";
 
 const App = () => {
   return (
-    <div className="login-container">
-      <h2 className="form-title">Log in with</h2>
-      <SocialLogin />
+    <>
+      <h1 className="chyrp-title">Chyrp Lite</h1>
+      <div className="login-container">
+        <h2 className="form-title">Log in with</h2>
+        <SocialLogin1 />
 
-      <p className="separator"><span>or</span></p>
+        <p className="separator"><span>or</span></p>
 
-      <form action="#" className="login-form">
-        <InputField type="email" placeholder="Email address" icon="mail" />
-        <InputField type="password" placeholder="Password" icon="lock" />
+        <form action="#" className="login-form">
+          <InputField type="email" placeholder="Email address" icon="mail" />
+          <InputField type="password" placeholder="Password" icon="lock" />
 
-        <a href="#" className="forgot-password-link">Forgot password?</a>
-        <button type="submit" className="login-button">Log In</button>
-      </form>
+          <a href="#" className="forgot-password-link">Forgot password?</a>
+          <button type="submit" className="login-button">Log In</button>
+        </form>
 
-      <p className="signup-prompt">
-        Don&apos;t have an account? <a href="#" className="signup-link">Sign up</a>
-      </p>
-    </div>
+        <p className="signup-prompt">
+          Don&apos;t have an account? <a href="#" className="signup-link">Sign up</a>
+        </p>
+      </div>
+    </>
   )
 }
 
